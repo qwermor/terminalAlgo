0d820e3c103d13ac2290f1c2bd29c4ec208dd6ac
qwermor <44215991+qwermor@users.noreply.github.com>
2018-10-17 21:49:20 -0600

    Update algo_strategy.py
    added late game improvements

--- algos/starter-algo-ZIPME/algo_strategy.py
+++ algos/starter-algo-ZIPME/algo_strategy.py
@@ -5,22 +5,6 @@ import warnings
 from sys import maxsize
 ATTACKED_lt = 0
 
-"""
-Most of the algo code you write will be in this file unless you create new
-modules yourself. Start by modifying the 'on_turn' function.
-
-Advanced strategy tips: 
-
-Additional functions are made available by importing the AdvancedGameState 
-class from gamelib/advanced.py as a replcement for the regular GameState class 
-in game.py.
-
-You can analyze action frames by modifying algocore.py.
-
-The GameState.map object can be manually manipulated to create hypothetical 
-board states. Though, we recommended making a copy of the map to preserve 
-the actual current map state.
-"""
 
 class AlgoStrategy(gamelib.AlgoCore):
     def __init__(self):
@@ -28,9 +12,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         random.seed()
 
     def on_game_start(self, config):
-        """ 
-        Read in config and perform any initial setup here 
-        """
+
         gamelib.debug_write('Configuring your custom algo strategy...')
         self.config = config
         global FILTER, ENCRYPTOR, DESTRUCTOR, PING, EMP, SCRAMBLER
@@ -44,37 +26,31 @@ class AlgoStrategy(gamelib.AlgoCore):
         ATTACKED_lt = 0
 
     def on_turn(self, turn_state):
-        """
-        This function is called every turn with the game state wrapper as
-        an argument. The wrapper stores the state of the arena and has methods
-        for querying its state, allocating your current resources as planned
-        unit deployments, and transmitting your intended deployments to the
-        game engine.
-        """
+
         game_state = gamelib.GameState(self.config, turn_state)
         gamelib.debug_write('Performing turn {} of your custom algo strategy'.format(game_state.turn_number))
         #game_state.suppress_warnings(True)  #Uncomment this line to suppress warnings.
 
         self.starter_strategy(game_state)
 
+
         game_state.submit_turn()
 
-    """
-    NOTE: All the methods after this point are part of the sample starter-algo
-    strategy and can safey be replaced for your custom algo.
-    """
+    
     def starter_strategy(self, game_state):
+        
 
         self.round1(game_state)
 
         self.buildBase(game_state)
-
 
         self.build_defences(game_state)
 
         self.deploy_SCRAMBLER(game_state)
 
         self.deploy_attackers(game_state)
+
+
     
     def round1(self, game_state):
         if (game_state.turn_number <= 1):
@@ -107,7 +83,7 @@ class AlgoStrategy(gamelib.AlgoCore):
                     game_state.attempt_spawn(FILTER, location)
             
 
-        firewall_locations = [[ 2, 13],[ 4, 12],[ 24, 12],[ 25, 12],[ 5, 11],[ 9, 11],[ 10, 11],[ 13, 11],[ 14, 11],[ 17, 11],[ 18, 11],[ 22, 11],[ 6, 10],[ 9, 10],[ 18, 10],[ 21, 10],[ 7, 9],[ 20, 9]]
+        firewall_locations = [[ 2, 13],[ 4, 12],[ 26, 12],[ 24, 12],[ 25, 12],[ 5, 11],[ 9, 11],[ 10, 11],[ 13, 11],[ 14, 11],[ 17, 11],[ 18, 11],[ 22, 11],[ 6, 10],[ 9, 10],[ 18, 10],[ 21, 10],[ 7, 9],[ 20, 9]]
         for location in firewall_locations:
             if game_state.can_spawn(DESTRUCTOR, location):
                 game_state.attempt_spawn(DESTRUCTOR, location) 
@@ -138,13 +114,25 @@ class AlgoStrategy(gamelib.AlgoCore):
             for location in firewall_locations:
                 if game_state.can_spawn(ENCRYPTOR, location):
                     game_state.attempt_spawn(ENCRYPTOR, location)
-
+        ##LATEGAME
+        if ((game_state.turn_number > 20)):
+            firewall_locations = [[ 26, 13],[ 27, 13],[ 25, 11]]
+            for location in firewall_locations:
+                if game_state.can_spawn(DESTRUCTOR, location):
+                    game_state.attempt_spawn(DESTRUCTOR, location)
+            if (game_state.get_resource(game_state.BITS) > 20):
+                firewall_locations = [[ 11, 8],[ 12, 8],[ 15, 8],[ 16, 8],[ 19, 8],[ 9, 7]]
+                for location in firewall_locations:
+                    if game_state.can_spawn(DESTRUCTOR, location):
+                        game_state.attempt_spawn(DESTRUCTOR, location)
     def deploy_SCRAMBLER(self, game_state):
         if (game_state.turn_number <= 10):
-            deploy_location = [[ 2, 11],[ 25, 11],[ 10, 3],[ 17, 3]]
+            deploy_location = [[ 4, 9],[ 23, 9],[ 10, 3],[ 17, 3]]
             for location in deploy_location:
                 if game_state.can_spawn(SCRAMBLER, location):
                     game_state.attempt_spawn(SCRAMBLER, location)
+
+
 
 
     def deploy_attackers(self, game_state):
@@ -156,7 +144,7 @@ class AlgoStrategy(gamelib.AlgoCore):
                     game_state.attempt_spawn(PING, [ 14, 0])
              
         
-        if (game_state.turn_number <= 10 or game_state.get_resource(game_state.BITS) < 6 + (int (game_state.turn_number / 10) * 3)):
+        if (game_state.turn_number <= 10 or game_state.get_resource(game_state.BITS) < 9 + (int (game_state.turn_number / 10) * 3)):
             return
         while game_state.can_spawn(EMP, [2, 11]):
             game_state.attempt_spawn(EMP, [2, 11])
